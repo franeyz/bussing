f6a97ecc4b729fa8726e1adc029b9c0f1ecdc9a4
franeyz <109262146+franeyz@users.noreply.github.com>
2024-04-27 11:00:47 -0400

    debug vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/nyu-csci-ua-0467-001-002-spring-2024/final-project-franeyz"
+  base: "https://franeyz.github.io/final-project-franeyz/"
 })
