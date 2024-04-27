f1f3b474984b28b2bcd0a635789eacbea6f2f472
franeyz <109262146+franeyz@users.noreply.github.com>
2024-04-26 22:26:08 -0400

    debug cors

--- server/routes/routes.mjs
+++ server/routes/routes.mjs
@@ -1,9 +1,14 @@
+import '../config.mjs';
+import dotenv from 'dotenv';
 import express from 'express';
 const router = express.Router();
 import cors from 'cors';
 import {test,registerUser,loginUser, getSchedules} from '../controllers/controller.mjs';
 
+dotenv.config();
 const origin = process.env.CLIENT ?? 'http://localhost:5173';
+
+console.log('CORS origin:', origin);
 
 router.use(
     cors({
